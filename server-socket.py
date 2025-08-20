eb5aaa30b167b5ff6869a7f247f7b4d0230ed51a
Carlos <davidcastro417@gmail.com>
2025-08-20 10:48:37 -0400

    Code question3

--- server-socket.py
+++ server-socket.py
@@ -1,17 +1,34 @@
+# server_multithread.py
 from socket import *
-import time
+import threading
 
 serverPort = 14000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(("",serverPort))
-serverSocket.listen(1)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(5)   # permite hasta 5 conexiones en cola
 print("The server is ready to receive")
+
+contador = 0
+lock = threading.Lock()
+
+def atender_cliente(connectionSocket, addr, client_id):
+    try:
+        print(f"[Cliente-{client_id}] Conectado desde {addr}")
+        sentence = connectionSocket.recv(1024).decode()
+        print(f"[Cliente-{client_id}] Mensaje recibido: {sentence}")
+        capitalizedSentence = sentence.upper()
+        connectionSocket.send(capitalizedSentence.encode())
+    except Exception as e:
+        print(f"[Cliente-{client_id}] Error: {e}")
+    finally:
+        connectionSocket.close()
+        print(f"[Cliente-{client_id}] Conexión cerrada")
+
 while True:
     connectionSocket, addr = serverSocket.accept()
-    print("From Client:", addr)
-    sentence = connectionSocket.recv(1024).decode()
-    print("I received:", sentence)
-    capitalizedSentence = sentence.upper()
-   # time.sleep(3)
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    with lock:
+        contador += 1
+        client_id = contador
+    
+    t = threading.Thread(target=atender_cliente, args=(connectionSocket, addr, client_id))
+    t.start()
